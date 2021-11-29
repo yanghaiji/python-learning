9dbf9b2e07911c1c0108a309002fcbd1b104b148
haiji.yang <haiji.yang@capgemini.com>
2021-11-29 13:21:22 +0800

    while 循环函数 :new:

---
+++ base/Range 函数.py
@@ -0,0 +1,13 @@
+# range
+
+if __name__ == '__main__':
+
+    for i in range(5):
+        print(i)
+
+    list_num = list(range(5, 10))
+    print(list_num)
+
+    print(list(range(0, 10, 3)))
+
+    print(sum(range(0, 101)))
---
+++ base/循环语句_while.py
@@ -0,0 +1,12 @@
+# while
+# 学习了for循环，今天我们来学习一下 while 循环
+
+if __name__ == '__main__':
+    # 循环输出10次hello python
+    nun = 1
+    while nun < 10:
+        print("Hello Python")
+        nun = nun + 1
+
+    print("----------")
+
