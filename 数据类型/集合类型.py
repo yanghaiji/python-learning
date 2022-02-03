9ca51b6090c6198cafd63f54630ec309c45f2e62
yanghaiji <hai.ji.yang@ibm.com>
2022-02-03 11:07:51 +0800

    list demo :new:

--- 数据类型/集合类型.py
+++ 数据类型/集合类型.py
@@ -25,8 +25,30 @@ print(user_name_list)
 # 添加
 user_name_list.append('grape')
 print(user_name_list)
-# 排序
-user_name_list.sort()
+# 添加2
+# extend 将指定集合中的元素追加到到另一个集合中
+# append 将当作一个整体进行添加，extend会解析
+user_pwd = ['hai', 'ji']
+user_name_list.extend(user_pwd)
 print(user_name_list)
+
 # 弹出一个
 print(user_name_list.pop())
+
+# 删除  del
+del user_pwd[0]
+print(user_pwd)
+# 删除  remove
+user_pwd.remove("ji")
+print(user_pwd)
+
+# 修改
+user_pwd.append('test')
+print(user_pwd)
+user_pwd[0] = 'update'
+print(user_pwd)
+
+# 排序 默认是升序，如想实现降序，可以在sort内添加reverse=True
+# user_name_list.sort(reverse=True)
+user_name_list.sort()
+print(user_name_list)
