ff3a5cfa1bbe95aaf4760f7ca7c2470a1bb4be8d
yanghaiji <hai.ji.yang@ibm.com>
2022-09-01 19:33:04 +0800

    update demo01

--- seleniums/demo01.py
+++ seleniums/demo01.py
@@ -1,10 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+# 获取chrome 驱动
 driver = webdriver.Chrome()
-
+# 访问地址
 driver.get("http://www.baidu.com")
-driver.find_element(By.XPATH, '//*[@id="kw"]').send_keys("python")
+# 元素的定位
+ele = driver.find_element(By.XPATH, '//*[@id="kw"]')
+ele.send_keys("python")
+# 点击搜索
 driver.find_element(By.XPATH, '//*[@id="su"]').click()
-
-driver.quit()
+# 退出
+# driver.quit()
